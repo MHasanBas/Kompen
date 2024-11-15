1518fa6a76a176b3405f8876615153800449eaca
Agate1234 <faizabiyu72@gmail.com>
2024-11-16 00:12:19 +0700

    🥶fitur register mas faiz

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import 'dosen/task_approval_page.dart'; // Import halaman dosen
-import 'dosen/dashboard.dart';
-import 'mahasiswa/home_page.dart' as mahasiswa; // Alias untuk halaman mahasiswa
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'mahasiswa/home_page.dart' as mahasiswa;
+import 'dosen/dashboard.dart' as dosen;
+import 'tendik/dashboard.dart' as tendik;
+import 'register.dart';
 
 class LoginPage extends StatelessWidget {
-  final TextEditingController nimController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
   LoginPage({super.key});
 
-  void showLoginResultDialog(BuildContext context, String message, bool isSuccess) {
+  void showLoginResultDialog(BuildContext context, String message, bool isSuccess, String levelKode) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -39,12 +41,28 @@ class LoginPage extends StatelessWidget {
                 onPressed: () {
                   Navigator.of(context).pop();
                   if (isSuccess) {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => isSuccess ? HomeScreen() : mahasiswa.HomePage(),
-                      ),
-                    );
+                    // Cek level pengguna dan arahkan ke halaman yang sesuai
+                    if (levelKode == 'MHS') {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => mahasiswa.HomePage()),
+                      );
+                    } else if (levelKode == 'DSN') {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => dosen.HomeScreen()),
+                      );
+                    } else if (levelKode == 'TDK') {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => tendik.HomeScreen()),
+                      );
+                    } else {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                    }
                   }
                 },
                 style: ElevatedButton.styleFrom(
@@ -63,28 +81,26 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-void login(BuildContext context) {
-  String nim = nimController.text;
-  String password = passwordController.text;
+  // Fungsi login ke API
+  Future<void> login(String username, String password, BuildContext context) async {
+    var url = 'http://192.168.69.40:8000/api/login'; // Ganti dengan URL API yang sesuai
+    var response = await http.post(Uri.parse(url), body: {
+      'username': username, 
+      'password': password,
+    });
 
-  if (nim == '12345' && password == 'password') {
-    showLoginResultDialog(context, 'Selamat datang, dosen!', true);
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => HomeScreen()),
-    );
-  } else if (nim == '67890' && password == 'password') {
-    showLoginResultDialog(context, 'Selamat datang, mahasiswa!', true);
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => mahasiswa.HomePage()), // Menggunakan alias
-    );
-  } else {
-    showLoginResultDialog(context, 'NIM/NIP atau password salah', false);
-    nimController.clear();
-    passwordController.clear();
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      // Pastikan data level ada dalam respons
+      String levelKode = data['user']['level_kode']; // Ambil level_kode
+      showLoginResultDialog(context, 'Selamat datang, ${data['user']['nama']}!', true, levelKode);
+    } else {
+      var data = jsonDecode(response.body);
+      showLoginResultDialog(context, 'Username atau password salah', false, '');
+      usernameController.clear();
+      passwordController.clear();
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
@@ -133,9 +149,9 @@ void login(BuildContext context) {
                     ),
                     padding: const EdgeInsets.symmetric(horizontal: 10),
                     child: TextField(
-                      controller: nimController,
+                      controller: usernameController,
                       decoration: const InputDecoration(
-                        hintText: "NIM/NIP",
+                        hintText: "Username", // Menggunakan username sebagai input
                         border: InputBorder.none,
                       ),
                       style: const TextStyle(fontSize: 18),
@@ -165,7 +181,7 @@ void login(BuildContext context) {
                   ElevatedButton(
                     onPressed: () {
                       FocusScope.of(context).unfocus();
-                      login(context);
+                      login(usernameController.text, passwordController.text, context);
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blue[900],
@@ -191,156 +207,6 @@ void login(BuildContext context) {
                     },
                     child: Text(
                       "Belum punya akun? Register",
-                      style: GoogleFonts.poppins(
-                        textStyle: TextStyle(
-                          fontSize: 16,
-                          color: Colors.blue[900],
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class RegisterPage extends StatelessWidget {
-  final TextEditingController nimController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final TextEditingController confirmPasswordController = TextEditingController();
-
-  RegisterPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 0, 30, 130),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30),
-                ),
-              ),
-              child: Center(
-                child: Text(
-                  "Register",
-                  style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 20),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      border: Border.all(
-                        color: Colors.blue[900]!,
-                        width: 2.0,
-                      ),
-                    ),
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: TextField(
-                      controller: nimController,
-                      decoration: const InputDecoration(
-                        hintText: "NIM/NIP",
-                        border: InputBorder.none,
-                      ),
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      border: Border.all(
-                        color: Colors.blue[900]!,
-                        width: 2.0,
-                      ),
-                    ),
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: TextField(
-                      controller: passwordController,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        hintText: "Password",
-                        border: InputBorder.none,
-                      ),
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      border: Border.all(
-                        color: Colors.blue[900]!,
-                        width: 2.0,
-                      ),
-                    ),
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: TextField(
-                      controller: confirmPasswordController,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        hintText: "Confirm Password",
-                        border: InputBorder.none,
-                      ),
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  const SizedBox(height: 40),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Logika untuk register
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue[900],
-                      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    child: const Text(
-                      "Register",
-                      style: TextStyle(fontSize: 16, color: Colors.white),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => LoginPage(),
-                        ),
-                      );
-                    },
-                    child: Text(
-                      "Sudah punya akun? Login",
                       style: GoogleFonts.poppins(
                         textStyle: TextStyle(
                           fontSize: 16,
--- lib/register.dart
+++ lib/register.dart
@@ -1,36 +1,93 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'login_page.dart';
 
-class RegisterPage extends StatelessWidget {
-  final TextEditingController nimController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final TextEditingController confirmPasswordController = TextEditingController();
+final dio = Dio();
 
+var all_data = [];
+
+final TextEditingController levelController = TextEditingController();
+final TextEditingController usernameController = TextEditingController();
+final TextEditingController namaController = TextEditingController();
+final TextEditingController passwordController = TextEditingController();
+final TextEditingController confirmPasswordController = TextEditingController();
+
+String url_domain = "http://192.168.18.30:8000";
+String url_create_data = url_domain + "/api/create_data";
+String url_get_levels = url_domain + "/api/levels";
+
+class RegisterPage extends StatefulWidget {
   RegisterPage({super.key});
 
-  // Fungsi untuk mendaftar ke API
-  Future<void> register(String nim, String password, String confirmPassword, BuildContext context) async {
-    var url = 'http://192.168.196.117:8000/api/register';
-    var response = await http.post(Uri.parse(url), body: {
-      'nim': nim,
-      'password': password,
-      'password_confirmation': confirmPassword, // Include password confirmation
-    });
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
 
-    if (response.statusCode == 201) {
-      var data = jsonDecode(response.body);
-      print('Registrasi berhasil: ${data['message']}');
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => LoginPage()),
+class _RegisterPageState extends State<RegisterPage> {
+  List<dynamic> levels = [];
+  int? selectedLevelId;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchLevels();
+  }
+
+  Future<void> fetchLevels() async {
+    try {
+      var response = await dio.get(url_get_levels);
+      print(response.data);
+      if (response.statusCode == 200 && response.data.containsKey('data')) {
+        setState(() {
+          levels = response.data['data'];
+        });
+      } else {
+        throw Exception('Invalid response structure');
+      }
+    } catch (e) {
+      print("Error fetching levels: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Gagal mengambil data level.')),
       );
-    } else {
-      var data = jsonDecode(response.body);
-      print('Registrasi gagal: ${data['message']}');
-      // Show error message
+    }
+  }
+
+  Future<void> register(int levelId, String username, String nama, String password, String confirmPassword, BuildContext context) async {
+    try {
+      var response = await dio.post(
+        url_create_data,
+        data: {
+          'level_id': levelId,
+          'username': username,
+          'nama': nama,
+          'password': password,
+        },
+      );
+
+      if (response.statusCode == 201 || response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Registrasi berhasil!')),
+        );
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginPage()),
+        );
+        usernameController.clear();
+        namaController.clear();
+        passwordController.clear();
+      } else {
+        String errorMessage = response.data['message'] ?? 'Registrasi gagal';
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(errorMessage)),
+        );
+      }
+    } catch (e) {
+      print("Error during registration: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Terjadi kesalahan saat registrasi.')),
+      );
     }
   }
 
@@ -46,8 +103,8 @@ class RegisterPage extends StatelessWidget {
               decoration: const BoxDecoration(
                 color: Color.fromARGB(255, 0, 30, 130),
                 borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30),
+                  bottomLeft: Radius.circular(60),
+                  bottomRight: Radius.circular(60),
                 ),
               ),
               child: Center(
@@ -66,11 +123,46 @@ class RegisterPage extends StatelessWidget {
           ),
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(12.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   const SizedBox(height: 20),
+                  DropdownButtonFormField<int>(
+                    value: selectedLevelId,
+                    items: levels.isEmpty
+                        ? []
+                        : levels.map<DropdownMenuItem<int>>((level) {
+                            return DropdownMenuItem<int>(
+                              value: level['level_id'],
+                              child: Text(level['level_nama']),
+                            );
+                          }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedLevelId = value!;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 10),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(
+                          color: Colors.blue[900]!,
+                          width: 2.0,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(
+                          color: Colors.blue[900]!,
+                          width: 2.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
                   Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(8.0),
@@ -81,15 +173,34 @@ class RegisterPage extends StatelessWidget {
                     ),
                     padding: const EdgeInsets.symmetric(horizontal: 10),
                     child: TextField(
-                      controller: nimController,
+                      controller: usernameController,
                       decoration: const InputDecoration(
-                        hintText: "NIM/NIP",
+                        hintText: "Username",
                         border: InputBorder.none,
                       ),
                       style: const TextStyle(fontSize: 18),
                     ),
                   ),
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 15),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(
+                        color: Colors.blue[900]!,
+                        width: 2.0,
+                      ),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: TextField(
+                      controller: namaController,
+                      decoration: const InputDecoration(
+                        hintText: "Nama",
+                        border: InputBorder.none,
+                      ),
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
                   Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(8.0),
@@ -109,7 +220,7 @@ class RegisterPage extends StatelessWidget {
                       style: const TextStyle(fontSize: 18),
                     ),
                   ),
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 15),
                   Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(8.0),
@@ -129,15 +240,34 @@ class RegisterPage extends StatelessWidget {
                       style: const TextStyle(fontSize: 18),
                     ),
                   ),
-                  const SizedBox(height: 40),
+                  const SizedBox(height: 25),
                   ElevatedButton(
                     onPressed: () {
-                      FocusScope.of(context).unfocus();
-                      register(nimController.text, passwordController.text, confirmPasswordController.text, context);
+                      if (selectedLevelId == null) {
+                        print("Level harus dipilih");
+                      } else if (usernameController.text.isEmpty ||
+                          namaController.text.isEmpty ||
+                          passwordController.text.isEmpty ||
+                          confirmPasswordController.text.isEmpty) {
+                        print("Semua field harus diisi");
+                      } else if (passwordController.text !=
+                          confirmPasswordController.text) {
+                        print("Password dan konfirmasi password tidak cocok");
+                      } else {
+                        register(
+                          selectedLevelId!,
+                          usernameController.text,
+                          namaController.text,
+                          passwordController.text,
+                          confirmPasswordController.text,
+                          context,
+                        );
+                      }
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blue[900],
-                      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 50, vertical: 15),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10),
                       ),
@@ -176,4 +306,21 @@ class RegisterPage extends StatelessWidget {
       ),
     );
   }
+}
+
+void create_data(int levelId, String username, String nama, String password) async {
+  Response response;
+  response = await dio.post(
+    url_create_data,
+    queryParameters: {
+      'level_id': levelId,
+      'username': username,
+      'nama': nama,
+      'password': password
+    },
+  );
+
+  usernameController.text = "";
+  namaController.text = "";
+  passwordController.text = "";
 }
